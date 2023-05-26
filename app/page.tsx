6bda5591341c6773acc3d87d3c0c4d008e50ea7c
iimpulsivedev <nomadicpickle@icloud.com>
2023-05-26 10:31:44 -0400

    messing around with styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,95 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="">
-      <p className="text-5xl">iimpulsive</p>
+    <main className="flex min-h-screen flex-col border-4 border-red-500">
+      <div className="min-w-screen bg-black text-center text-white">
+        <p className="p-5 text-3xl">Header</p>
+      </div>
+      <div className="flex-grow border-4 border-blue-500 py-5">
+        <div className="flex flex-wrap justify-center">
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+        </div>
+        <div className="flex flex-wrap justify-center">
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+        </div>
+        <div className="flex flex-wrap justify-center">
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+        </div>
+        <div className="flex flex-wrap justify-center">
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+        </div>
+        <div className="flex flex-wrap justify-center">
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+        </div>
+        <div className="flex flex-wrap justify-center">
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+        </div>
+        <div className="flex flex-wrap justify-center">
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+        </div>
+        <div className="flex flex-wrap justify-center">
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+        </div>
+        <div className="flex flex-wrap justify-center">
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+        </div>
+      </div>
+      <div className="min-w-screen text-screen bg-black text-center text-white">
+        <p className="p-5 text-3xl">Footer</p>
+      </div>
     </main>
   );
 }
