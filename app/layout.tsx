c80fed1dde687efa31b9a6b370722c6bac40f3fe
iimpulsivedev <nomadicpickle@icloud.com>
2023-05-30 11:55:26 -0400

    fixed spelling of agencies in meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 export const metadata = {
   title: "Noah Pickle | Software Engineer",
   description:
-    "Hello World, My name is Noah Pickle, aka !mpulsive. I am a software developer with over 4 years of experience building web applications and websites for a military contractor and insurance agencys. Technologies that I am familiar with are typescript, php, MySQL.",
+    "Hello World, My name is Noah Pickle, aka !mpulsive. I am a software developer with over 4 years of experience building web applications and websites for a military contractor and insurance agencies. Technologies that I am familiar with are typescript, php, MySQL.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
