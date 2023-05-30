716f7be333f1df37ffc5e39099e7f1e60a9a56a2
iimpulsivedev <nomadicpickle@icloud.com>
2023-05-30 00:03:18 -0400

    added main personal site page with resume & pfp

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,75 @@
+import Image from "next/image";
+import Button from "@/components/button";
+import Card from "@/components/card";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col border-4 border-red-500">
-      <div className="min-w-screen bg-black text-center text-white">
-        <p className="p-5 text-3xl">Header</p>
-      </div>
-      <div className="flex-grow border-4 border-blue-500 py-5">
-        <div className="flex flex-wrap justify-center">
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+    <main className="flex min-h-screen flex-col bg-slate-800">
+      <div className="flex-grow py-5">
+        <div className="flex flex-col items-center">
+          <p className="mb-8 text-center text-6xl font-bold text-white">Noah Pickle</p>
+          <Image
+            src="/noah-pickle.JPEG"
+            alt="picture of noah pickle"
+            width={300}
+            height={300}
+          />
         </div>
-        <div className="flex flex-wrap justify-center">
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+        <div>
+          <p className="my-8 text-center text-5xl font-bold text-white">Resume</p>
+          <div className="flex justify-center py-5">
+            <a
+              href="./Noah_Pickle_Resume.pdf"
+              target="_blank"
+            >
+              <Button text="View" />
+            </a>
+            <a
+              href="./Noah_Pickle_Resume.pdf"
+              download
+            >
+              <Button text="Download" />
+            </a>
+          </div>
         </div>
-        <div className="flex flex-wrap justify-center">
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+        <div>
+          <p className="my-8 text-center text-5xl font-bold text-white">Projects</p>
+          <div className="flex flex-col items-center justify-center py-5 md:flex-row md:flex-wrap">
+            <Card
+              title="Yoorblog"
+              desc="Yoorblog is an open source alternative to wordpress CMS built from Next.js, Typescript,
+              React and Prisma."
+              link={{
+                project: "https://yoorblog.com",
+                source: "https://github.com/iimpulsivedev/yoorblog",
+              }}
+            />
+          </div>
         </div>
-        <div className="flex flex-wrap justify-center">
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+        <div>
+          <p className="my-8 text-center text-5xl font-bold text-white">Skills</p>
+          <div className="flex flex-col items-center justify-center py-5 md:flex-row md:flex-wrap">
+            <Card title="Project Management" />
+            <Card title="Typescript" />
+            <Card title="React" />
+            <Card title="SCSS" />
+            <Card title="PHP" />
+            <Card title="MySQL" />
+            <Card title="Database Design" />
+            <Card title="Database Administration" />
+          </div>
         </div>
-        <div className="flex flex-wrap justify-center">
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
+        <div>
+          <p className="my-8 text-center text-5xl font-bold text-white">Tools</p>
+          <div className="flex flex-col items-center justify-center py-5 md:flex-row md:flex-wrap">
+            <Card title="VSCode" />
+            <Card title="WSL2" />
+            <Card title="Docker" />
+            <Card title="Git" />
+            <Card title="Github" />
+            <Card title="DBeaver" />
+          </div>
         </div>
-        <div className="flex flex-wrap justify-center">
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-        </div>
-        <div className="flex flex-wrap justify-center">
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-        </div>
-        <div className="flex flex-wrap justify-center">
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-        </div>
-        <div className="flex flex-wrap justify-center">
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-          <p className="mx-4 my-6 border-2 border-green-500 p-4 text-5xl">iimpulsive</p>
-        </div>
-      </div>
-      <div className="min-w-screen sticky bottom-0 bg-black text-center text-white">
-        <p className="p-5 text-3xl">Footer</p>
       </div>
     </main>
   );
